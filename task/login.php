3f7e9768da2e96ca16402892627edeeb58084663
Nirajan Shakya <nirajanshakya746@gmail.com>
2017-08-23 12:48:33 +0545

    Login form updated

--- task/login.php
+++ task/login.php
@@ -4,6 +4,12 @@
 
 </head>
 <body>
-
+<link rel='stylesheet' href='style.css'>
+<form>
+<div class = "Login">
+<input type="text" placeholder="Username" name="username" required>
+<input type="password" placeholder="Password" name ="password" required>
+<input type = "submit" value="Log In">
+</form>
 </body>
 </html>
